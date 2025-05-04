45cace3c770618976d4268cd79013159a9f86366
xxraihann <raihanananda1980@gmail.com>
2025-05-04 19:11:48 +0700

    menambahkan main

---
+++ AljabarConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AljabarLibraries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] akar = Aljabar.AkarPersamaanKuadrat(new double[] { 1, -3, -10 });
+        Console.WriteLine("Akar-akar persamaan kuadrat:");
+        foreach (double a in akar)
+        {
+            Console.WriteLine(a);
+        }
+
+        double[] hasilKuadrat = Aljabar.HasilKuadrat(new double[] { 2, -3 });
+        Console.WriteLine("\nHasil kuadrat dari 2x - 3:");
+        foreach (double h in hasilKuadrat)
+        {
+            Console.WriteLine(h);
+        }
+
+        Console.ReadLine();
+    }
+}
